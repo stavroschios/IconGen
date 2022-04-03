8f79fa056362a835eff8ad544388cbd8a9d31fe8
Stavros Pachoundakis <stavros@livescale.tv>
2022-04-03 00:10:01 -0400

    first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Stavros Pachoundakis on 2022-04-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/IconGeneratorApp.swift
@@ -0,0 +1,16 @@
+//
+//  IconGeneratorApp.swift
+//  Shared
+//
+//  Created by Stavros Pachoundakis on 2022-04-02.
+//
+import SwiftUI
+
+@main
+struct IconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
